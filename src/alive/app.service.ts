69ec70ce623bb0512a2b2cac64d03b2f2f544285
diegoscastanho <64171436+diegoscastanho@users.noreply.github.com>
2023-03-05 11:12:41 -0300

    feat: :sparkles: add version

--- src/alive/app.service.ts
+++ src/alive/app.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
+import * as pack from '../../package.json';
+
 @Injectable()
 export class AppService {
     getAlive(): object {
+        console.log(pack.version);
         return {
             message: "I am alive!",
-            version: "1.0.0"
+            version: pack.version
         };
     }
 
     getHello(): object {
+        // console.log(`Versão do NestJS: ${packageJson.version}`);
+        console.log(pack.version);
+
         return {
             message: "Hello World!",
-            version: "1.0.0"
+            version: pack.version
         };
     }
 }
